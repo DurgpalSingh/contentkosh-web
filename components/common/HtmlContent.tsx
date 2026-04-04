57b3ec2d3f48eb23375da96b1ceeb67024286747
Shubh404-SE <tanwarshubham29@gmail.com>
2026-04-04 15:05:25 +0530

    config for quill

--- components/common/HtmlContent.tsx
+++ components/common/HtmlContent.tsx
@@ -2,45 +2,15 @@
 
 import { useMemo } from 'react';
 import DOMPurify from 'dompurify';
-
-const quillAllowedTags = [
-  'p',
-  'br',
-  'strong',
-  'b',
-  'em',
-  'i',
-  'u',
-  's',
-  'h1',
-  'h2',
-  'h3',
-  'blockquote',
-  'ol',
-  'ul',
-  'li',
-  'a',
-  'span',
-];
-
-const quillAllowedAttributes: Record<string, string[]> = {
-  a: ['href', 'target', 'rel'],
-  span: ['class'],
-  p: ['class'],
-};
+import { QUIll_HTML_DOM_PURIFY_CONFIG } from '@/lib/config/quillHtmlDomPurifyConfig';
 
 export function HtmlContent({ html }: { html: string | null | undefined }) {
   const sanitizedHtml = useMemo(() => {
     const input = html ?? '';
-    return DOMPurify.sanitize(input, {
-      ALLOWED_TAGS: quillAllowedTags,
-      ALLOWED_ATTR: quillAllowedAttributes,
-      ALLOWED_URI_REGEXP: /^(?:(?:https?):|[^a-z]|[a-z]|[A-Z]|[0-9]|[$_.+!*'(),]|(?:%[0-9a-fA-F]{2}))+/,
-    });
+    return DOMPurify.sanitize(input, QUIll_HTML_DOM_PURIFY_CONFIG);
   }, [html]);
 
   if (!sanitizedHtml) return null;
 
   return <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
 }
-
