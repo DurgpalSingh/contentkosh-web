291af610cdabad58b7aa10f92330f0ff31781e6c
Shubh404-SE <tanwarshubham29@gmail.com>
2026-08-31 23:31:16 +0530

    policy + terms pages

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import ContentPage from '@/components/common/ContentPage';
+
+export default function PrivacyPolicyPage() {
+  return <ContentPage contentPath="/content/privacy-policy.json" />;
+}
